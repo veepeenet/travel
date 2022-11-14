cf660d0200b0184ed3f811a7a65fe245118e0cf0
veepee <veepeenet@outlook.com>
2022-11-13 21:32:44 -0600

    all

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development'
+}
+
+
+
+
+
